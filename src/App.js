58f1fdbc36306b842ccd75abea04f91769f08552
LeandroNegretti <negrettileandro3@gmail.com>
2024-09-12 20:50:49 -0300

    update: toast instalado e adicionado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
+import UsuarioForm from "./components/UsuarioForm";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import './toast.css';
 
 function App() {
   return (
@@ -9,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/usuarioForm" element={<UsuarioForm /> } />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
